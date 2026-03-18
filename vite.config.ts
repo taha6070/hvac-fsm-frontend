e2258aefb260962534292c9871f51887ffe4be23
taha6070 <mtaha.ya234@gmail.com>
2026-03-18 15:57:01 -0700

    packages update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
+  server: {
+    allowedHosts: ['erp.funnelszone.duckdns.org']
+  }
 });
